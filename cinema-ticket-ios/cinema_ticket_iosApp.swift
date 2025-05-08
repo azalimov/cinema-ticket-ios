22d30b5c693a36f230b324c554ada63ee85426a8
Azamat Alimov <retq@dsquared.local>
2025-05-08 12:28:33 +0500

    Initial commit

---
+++ cinema-ticket-ios/cinema_ticket_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinema_ticket_iosApp.swift
+//  cinema-ticket-ios
+//
+//  Created by Azamat Alimov on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct cinema_ticket_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
